46c6635957cbafb5b5b3dd1d70477efd7243693a
Maksymilian Walicki <maxwalicki@gmail.com>
2024-12-12 12:31:00 +0100

    [Frontend] - fixed useGetFetch to fetch only if there is no user's data.

--- frontend/construction-company-ms/src/hooks/useGetFetch.tsx
+++ frontend/construction-company-ms/src/hooks/useGetFetch.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { logout } from "../store/slices/authSlice";
 import { useNavigate } from "react-router";
+import { UserData } from "../utils/types";
 
 // implemented url data fetching hook to prevent copying code
 const useGetFetch = (url: string) => {
@@ -10,34 +11,38 @@ const useGetFetch = (url: string) => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const { token } = useSelector((state: RootState) => state.auth);
+  const user: UserData = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-    (() => {
-      console.log("Hello");
-      setLoading(true);
-      fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((res) => {
-          if (res.status !== 200) {
-            dispatch(logout());
-            navigate("/auth");
-          }
-          return res.json();
+    if (!user) {
+      // only fetch if there is no user data
+      (() => {
+        console.log("Hello");
+        setLoading(true);
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
         })
-        .then((data) => {
-          console.log("AAAA: ", data);
-          setData(data);
-          setLoading(false);
-        })
-        .catch((err) => setError(err));
-    })(); // self invoking function :)
+          .then((res) => {
+            if (res.status !== 200) {
+              dispatch(logout());
+              navigate("/auth");
+            }
+            return res.json();
+          })
+          .then((data) => {
+            console.log("AAAA: ", data);
+            setData(data);
+            setLoading(false);
+          })
+          .catch((err) => setError(err));
+      })(); // self invoking function :)
+    }
   }, []);
 
   return { data, error, loading };
